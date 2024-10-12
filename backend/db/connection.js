955ef5458b19f8b2a181faaa330adbd06e65cf7c
Dennis Wang <66754085+hellolol2016@users.noreply.github.com>
2024-10-12 11:58:04 -0400

    made imports windows friendly for backend

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -1,37 +1,40 @@
-require('dotenv').config({ path: './keys.env' });
-const mongoose = require('mongoose');
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
-const DBNAME = "HackHarvard"
+// Load environment variables from keys.env
+dotenv.config({ path: "./keys.env" });
+
+const DBNAME = "HackHarvard";
 const URI = `mongodb+srv://timtak:${process.env.MONGOPASSWORD}@learningcluster.uvkbeii.mongodb.net/${DBNAME}`;
 
 class MongoDBService {
-	constructor() {
-		this.connected = false;
-	}
+  constructor() {
+    this.connected = false;
+  }
 
-	async connect() {
-		if (!this.connected) {
-			try {
-				await mongoose.connect(URI, {
-					useNewUrlParser: true,
-					useUnifiedTopology: true,
-				});
-				console.log('[mongodb] connected to mongodb via mongoose');
-				this.connected = true;
-			} catch (error) {
-				console.error('[mongodb] error connecting to mongodb:', error);
-				this.connected = false;
-			}
-		}
-	}
+  async connect() {
+    if (!this.connected) {
+      try {
+        await mongoose.connect(URI, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        });
+        console.log("[mongodb] connected to mongodb via mongoose");
+        this.connected = true;
+      } catch (error) {
+        console.error("[mongodb] error connecting to mongodb:", error);
+        this.connected = false;
+      }
+    }
+  }
 
-	async close() {
-		if (this.connected) {
-			await mongoose.connection.close();
-			console.log('[mongodb] mongodb connection closed');
-			this.connected = false;
-		}
-	}
+  async close() {
+    if (this.connected) {
+      await mongoose.connection.close();
+      console.log("[mongodb] mongodb connection closed");
+      this.connected = false;
+    }
+  }
 }
 
-module.exports = MongoDBService;
+export default MongoDBService;
--- backend/db/schema.js
+++ backend/db/schema.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose');
+import mongoose from "mongoose";
 
 const errorSchema = new mongoose.Schema({
-	uniqueId: { type: String, required: true, unique: true },
-  username: { type: String, required: true, unique: false},
-	errorData: mongoose.Schema.Types.Mixed,
-	timestamp: { type: Date, default: Date.now },
+  uniqueId: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: false },
+  errorData: mongoose.Schema.Types.Mixed,
+  timestamp: { type: Date, default: Date.now },
 });
 
-const ErrorLog = mongoose.model('ErrorLog', errorSchema);
+const ErrorLog = mongoose.model("ErrorLog", errorSchema);
 
-module.exports = ErrorLog;
+export default ErrorLog;
